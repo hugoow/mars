d5dcf5653b6a5b576fd4390f5385ea59a01c0adc
hugoow <83821317+hugoow@users.noreply.github.com>
2022-06-03 23:10:34 -0300

    Add files via upload

---
+++ docs/js/index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    el_autohide = document.querySelector('.autohide');
+    mob = document.querySelector('.mobile')
+    
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+  /*  document.body.style.paddingTop = navbar_height + 'px'; */
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+      // window.addEventListener
+    }
+    // if
+  
+  }); 
+
+
+  var nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 50) {
+      nav.classList.add('fundonav', 'shadow');
+    } else {
+      nav.classList.remove('fundonav', 'shadow',);
+    }
+  });
